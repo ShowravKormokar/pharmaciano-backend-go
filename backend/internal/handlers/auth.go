788d6065cedd3a16ecfa320fda678fc7ae8e6842
ShowravKormokar <showravkormokar354@gmail.com>
2026-01-12 00:20:36 +0600

    Fix import issue

--- backend/internal/handlers/auth.go
+++ backend/internal/handlers/auth.go
@@ -3,11 +3,31 @@ package handlers
 import (
 	"net/http"
 
+	"backend/internal/auth"
+	"backend/internal/config"
+
 	"github.com/gin-gonic/gin"
 )
 
 func Login(c *gin.Context) {
+	// demo user (later DB)
+	userID := uint(1)
+	role := "admin"
+
+	token, err := auth.GenerateAccessToken(
+		userID,
+		role,
+		config.Cfg.JWT.Secret,
+		config.Cfg.JWT.AccessTTL,
+	)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "failed to generate token",
+		})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{
-		"message": "login ok",
+		"access_token": token,
 	})
 }
