55078b9bc12c4e8fb33fe5348a4e3681b3616340
ShowravKormokar <showravkormokar354@gmail.com>
2026-08-16 20:25:04 +0600

    feat[api]: add ping route and test redis connection

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,18 +1,52 @@
 ﻿package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"backend/internal/platform/config"
+	"backend/internal/platform/db"
+	"backend/internal/platform/logger"
+	"backend/internal/platform/redis"
+	"context"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	// Context
+	ctx := context.Background()
+
+	// Config load
+	cfg := config.MustLoad(config.LoadOptions{})
+
+	// Logger initialize
+	log, err := logger.New(cfg.Logging, cfg.App.Name, cfg.App.Version)
+	if err != nil {
+		panic("logger init failed: " + err.Error())
+	}
+
+	// PostgreSQL connection test
+	pg, err := db.New(ctx, cfg.Database, log)
+	if err != nil {
+		log.Fatal("postgres: " + err.Error())
+	}
+	defer pg.Close()
+
+	// Redis connection test
+	rdb, err := redis.New(ctx, cfg.Redis, log)
+	if err != nil {
+		log.Fatal("redis: " + err.Error())
+	}
+	defer rdb.Close()
+
+	log.Info("✅ All platform connections OK")
+
+	// Gin router setup
 	r := gin.Default()
 
+	// Simple test route
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(
-			200,
-			gin.H{
-				"message": "pong!",
-			},
-		)
+		c.JSON(200, gin.H{"message": "pong!"})
 	})
 
+	// Run server
 	r.Run(":8080")
 }
