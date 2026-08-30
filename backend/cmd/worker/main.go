b40cef9bfa3424b58b0024f3c444e74e66b6f540
ShowravKormokar <showravkormokar354@gmail.com>
2026-08-30 11:31:50 +0600

    feat[woker]: init worker for dev

--- backend/cmd/worker/main.go
+++ backend/cmd/worker/main.go
@@ -1,1 +1,19 @@
 ﻿package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	log.Println("Pharmaciano worker starting... (stub)")
+
+	// Simulate running until SIGTERM/SIGINT
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+
+	log.Println("Worker shutting down...")
+}
